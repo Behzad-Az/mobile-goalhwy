9faa85a9bb9f7b7713554f25015b567e267c4383
Behzad-Az <behy1368@gmail.com>
2017-02-16 13:15:38 -0800

    added text fields to new doc form

--- CoursePage/NewDocForm.js
+++ CoursePage/NewDocForm.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Modal, Text, TouchableHighlight, TouchableOpacity, View, Image, TextInput, Picker } from 'react-native';
+import {
+  StyleSheet,
+  Modal,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Picker
+} from 'react-native';
 
 import { FontAwesome } from '@exponent/vector-icons';
 
@@ -7,12 +15,51 @@ class NewDocForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalVisible: false
-    }
+      modalVisible: false,
+      title: '',
+      type: '',
+      rev_desc: 'New Upload',
+      file_path: ''
+    };
+    this.setModalVisible = this.setModalVisible.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.handleNewDocPost = this.handleNewDocPost.bind(this);
   }
 
   setModalVisible(visible) {
     this.setState({modalVisible: visible});
+  }
+
+  handleChange(e) {
+    let state = {};
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
+  validateForm() {
+    return this.state.title &&
+           this.state.rev_desc &&
+           this.state.file_path &&
+           this.state.type
+  }
+
+  handleNewDocPost() {
+    console.log("i'm here 1: ", this.state);
+    // $.ajax({
+    //   method: 'POST',
+    //   url: `/api/courses/${this.props.courseId}`,
+    //   data: this.state,
+    //   success: response => {
+    //     if (response) {
+    //       this.reactAlert.showAlert("New document saved", "info");
+    //       HandleModal('new-doc-form');
+    //       this.props.reload(this.props.courseId);
+    //     } else {
+    //       this.reactAlert.showAlert("error in uploading document", "error");
+    //     }
+    //   }
+    // });
   }
 
   render() {
@@ -25,33 +72,53 @@ class NewDocForm extends Component {
           onRequestClose={() => {alert("Modal has been closed.")}}
         >
           <View style={styles.modalContainer}>
-            <View>
-              <Text style={styles.modalHeader}>New Document Form:</Text>
 
+            <Text style={styles.modalHeader}>New Document Form:</Text>
+
+            <View style={{marginTop: 20, borderWidth: .5, borderRadius: 5, paddingLeft: 10, paddingRight: 10}}>
+              <Text style={styles.inputLabel}>Document Title:</Text>
               <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                onChangeText={(text) => this.setState({text})}
-                value={this.state.text}
+                style={{height: 40, paddingRight: 5, paddingLeft: 5}}
+                onChangeText={title => this.setState({title})}
+                value={this.state.title}
+                placeholder="Example: Lab 1 - Electromagnetism"
               />
-
-              <Picker
-                selectedValue={this.state.language}
-                onValueChange={(lang) => this.setState({language: lang})}>
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
-              </Picker>
-
-              <TouchableHighlight onPress={() => this.setModalVisible(!this.state.modalVisible)}>
-                <Text>Go back</Text>
-              </TouchableHighlight>
-
             </View>
+
+            <View style={{marginTop: 20, borderWidth: .5, borderRadius: 5, paddingLeft: 10, paddingRight: 10}}>
+              <Text style={styles.inputLabel}>Revision Comment:</Text>
+              <TextInput
+                style={{height: 40, paddingRight: 5, paddingLeft: 5}}
+                onChangeText={rev_desc => this.setState({rev_desc})}
+                value={this.state.rev_desc}
+                placeholder="Example: New Upload"
+              />
+            </View>
+
+
+            <View style={{marginTop: 20, borderWidth: .5, borderRadius: 5}}>
+              <Picker
+                selectedValue={this.state.type}
+                onValueChange={type => this.setState({type})}
+                style={{color: '#004E89'}}>
+                <Picker.Item label="Select Type of Document:" value="" />
+                <Picker.Item label="Assignment / Report" value="asg_report" />
+                <Picker.Item label="Lecture Note" value="lecture_note" />
+                <Picker.Item label="Sample Question" value="sample_question" />
+              </Picker>
+            </View>
+
+            <Text onPress={() => this.setModalVisible(!this.state.modalVisible)}>Go back</Text>
+
           </View>
+
+
+
         </Modal>
 
-      <TouchableHighlight onPress={() => this.setModalVisible(true)}>
-        <Text style={styles.primaryBtn}><FontAwesome name="upload" size={19} color="white" /></Text>
-      </TouchableHighlight>
+        <Text style={styles.primaryBtn} onPress={() => this.setModalVisible(true)}>
+          <FontAwesome name="upload" size={19} color="white" />
+        </Text>
 
       </View>
     );
@@ -62,13 +129,18 @@ export default NewDocForm;
 
 const styles = StyleSheet.create({
   modalContainer: {
-    flex: 1,
-    backgroundColor: '#ddd',
     margin: 10,
     borderRadius: 10,
     padding: 10
   },
   modalHeader: {
+    color: '#004E89',
+    fontWeight: 'bold',
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#004E89'
+  },
+  inputLabel: {
     color: '#004E89',
     fontWeight: 'bold'
   },
