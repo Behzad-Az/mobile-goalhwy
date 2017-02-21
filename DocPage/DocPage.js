982325852c74cf6d7cacf4a3e7604af215cf60b2
Behzad-Az <behy1368@gmail.com>
2017-02-20 19:29:17 -0800

    added activity indicator to doc page

--- DocPage/DocPage.js
+++ DocPage/DocPage.js
@@ -4,7 +4,8 @@ import {
   Text,
   View,
   ScrollView,
-  Dimensions
+  Dimensions,
+  ActivityIndicator
 } from 'react-native';
 
 import { FontAwesome } from '@exponent/vector-icons';
@@ -18,6 +19,8 @@ class DocPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dataLoaded: false,
+      pageError: false,
       courseInfo: {
         id: this.props.courseId
       },
@@ -29,6 +32,7 @@ class DocPage extends React.Component {
     };
     this.loadComponentData = this.loadComponentData.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.renderPageAfterData = this.renderPageAfterData.bind(this);
   }
 
   componentDidMount() {
@@ -38,12 +42,55 @@ class DocPage extends React.Component {
   loadComponentData(courseId, docId) {
     fetch(`http://127.0.0.1:19001/api/courses/${courseId}/docs/${docId}`)
     .then(response => response.json())
-    .then(resJSON => resJSON ? this.setState(resJSON) : console.log("server error - 0", resJSON))
-    .catch(err => console.log("Error here: ", err));
+    .then(resJSON => {
+      resJSON.dataLoaded = true;
+      this.setState(resJSON)
+    })
+    .catch(err => {
+      console.log("Error here: DocPage.js: ", err);
+      this.setState({ dataLoaded: true, pageError: false });
+    });
   }
 
   handleSearch(searchResults) {
     this.setState({ searchResults });
+  }
+
+  renderPageAfterData() {
+    if (this.state.dataLoaded && this.state.pageError) {
+      return (
+        <View style={styles.componentContainer}>
+          <Text style={{padding: 5, textAlign: 'center'}}>
+            <FontAwesome name="exclamation-triangle" size={19}/> Error in loading up the page.
+          </Text>
+        </View>
+      );
+    } else if (this.state.dataLoaded) {
+      return (
+        <View style={{backgroundColor: 'white'}}>
+          <View style={styles.componentContainer}>
+            <Text style={styles.header}>{this.state.doc.title}</Text>
+            <TopRow courseInfo={this.state.courseInfo} />
+          </View>
+          <View style={styles.componentContainer}>
+            <Text style={styles.header}>Revisions:</Text>
+            { this.state.doc.revisions.map((rev, index) => <RevisionRow key={index} rev={rev} />) }
+            { !this.state.doc.revisions[0] && <Text style={{padding: 5}}>No revision could be found...</Text> }
+          </View>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.componentContainer}>
+          <ActivityIndicator
+            animating={true}
+            style={{height: 80}}
+            size={60}
+            color="#004E89"
+          />
+        </View>
+      );
+    }
   }
 
   render() {
@@ -56,16 +103,8 @@ class DocPage extends React.Component {
             { this.state.searchResults }
           </View>
 
-          <View style={styles.componentContainer}>
-            <Text style={styles.header}>{this.state.doc.title}</Text>
-            <TopRow courseInfo={this.state.courseInfo} />
-          </View>
+          { this.renderPageAfterData() }
 
-          <View style={styles.componentContainer}>
-            <Text style={styles.header}>Revisions:</Text>
-            { this.state.doc.revisions.map((rev, index) => <RevisionRow key={index} rev={rev} />) }
-            { !this.state.doc.revisions[0] && <Text style={{padding: 5}}>No revision could be found...</Text> }
-          </View>
         </View>
       </ScrollView>
     );
