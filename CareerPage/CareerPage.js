59d28885be7a84e4000c3fce5d7ca448429efb9a
Behzad-Az <behy1368@gmail.com>
2017-02-20 20:05:22 -0800

    added activity indicator to career page

--- CareerPage/CareerPage.js
+++ CareerPage/CareerPage.js
@@ -4,8 +4,11 @@ import {
   Text,
   View,
   ScrollView,
-  Dimensions
+  Dimensions,
+  ActivityIndicator
 } from 'react-native';
+
+import { FontAwesome } from '@exponent/vector-icons';
 
 import Navbar from '../Navbar/Navbar.js';
 import SearchBar from '../Partials/SearchBar.js';
@@ -17,12 +20,15 @@ class CareerPage extends React.Component {
     super(props);
     this.userId = this.props.userId || 1;
     this.state = {
+      dataLoaded: false,
+      pageError: false,
       jobs: [],
       searchResults: []
     };
     this.conditionData = this.conditionData.bind(this);
     this.loadComponentData = this.loadComponentData.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.renderPageAfterData = this.renderPageAfterData.bind(this);
   }
 
   componentDidMount() {
@@ -32,22 +38,61 @@ class CareerPage extends React.Component {
   loadComponentData() {
     fetch(`http://127.0.0.1:19001/api/users/${this.userId}/jobs`)
     .then(response => response.json())
-    .then(resJSON => resJSON ? this.conditionData(resJSON) : console.log("server error - 0", resJSON))
-    .catch(err => console.log("Error here: ", err));
+    .then(resJSON => this.conditionData(resJSON))
+    .catch(err => {
+      console.log("Error here: CareerPage.js: ", err);
+      this.setState({ dataLoaded: true, pageError: true });
+    });
   }
 
   conditionData(resJSON) {
-    let jobs = resJSON.map(data => {
-      return {
-        ...data._source.pin,
-        tags: data._source.pin.search_text.split(' ')
-      };
-    });
-    this.setState({ jobs });
+    if (resJSON) {
+      let jobs = resJSON.map(data => {
+        return { ...data._source.pin, tags: data._source.pin.search_text.split(' ') };
+      });
+      this.setState({ jobs, dataLoaded: true });
+    } else {
+      console.log("Error here: CareerPage.js: ", err);
+      this.setState({ dataLoaded: true, pageError: true });
+    }
   }
 
   handleSearch(searchResults) {
     this.setState({ searchResults });
+  }
+
+  renderPageAfterData() {
+    if (this.state.dataLoaded && this.state.pageError) {
+      return (
+        <View style={styles.componentContainer}>
+          <Text style={{padding: 5, textAlign: 'center'}}>
+            <FontAwesome name="exclamation-triangle" size={19}/> Error in loading up the page.
+          </Text>
+        </View>
+      );
+    } else if (this.state.dataLoaded) {
+      return (
+        <View style={{backgroundColor: 'white'}}>
+          <JobSearchForm reload={this.loadComponentData} />
+          <View style={styles.componentContainer}>
+            <Text style={styles.header}>Open Positions:</Text>
+            { this.state.jobs.map((job, index) => <JobRow key={index} job={job} />) }
+            { !this.state.jobs[0] && <Text style={{padding: 5, textAlign: 'center'}}>No jobs matching your search. Please revise your search criteria.</Text> }
+          </View>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.componentContainer}>
+          <ActivityIndicator
+            animating={true}
+            style={{height: 80}}
+            size={60}
+            color="#004E89"
+          />
+        </View>
+      );
+    }
   }
 
   render() {
@@ -61,13 +106,7 @@ class CareerPage extends React.Component {
             { this.state.searchResults }
           </View>
 
-          <JobSearchForm reload={this.loadComponentData} />
-
-          <View style={styles.componentContainer}>
-            <Text style={styles.header}>Open Positions:</Text>
-            { this.state.jobs.map((job, index) => <JobRow key={index} job={job} />) }
-            { !this.state.jobs[0] && <Text style={{padding: 5, textAlign: 'center'}}>No jobs matching your search. Please revise your search criteria.</Text> }
-          </View>
+          { this.renderPageAfterData() }
 
         </View>
       </ScrollView>
