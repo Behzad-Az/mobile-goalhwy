3541480793f57ef4a76db967a0f140a6a341d4d2
Behzad-Az <behy1368@gmail.com>
2017-02-25 19:31:25 -0800

    course review page components now match ios app - date picker is different in andriod vs ios

--- CourseReviewPage/NewCourseReviewForm.js
+++ CourseReviewPage/NewCourseReviewForm.js
@@ -11,8 +11,8 @@ import {
 } from 'react-native';
 
 import RadioInput from '../Partials/RadioInput.js';
-import AndroidDatePicker from '../Partials/AndroidDatePicker';
 import AutoCompleteTextInput from '../Partials/AutoCompleteTextInput.js';
+import AndroidDatePicker from '../Partials/AndroidDatePicker';
 
 import { FontAwesome } from '@exponent/vector-icons';
 
@@ -24,14 +24,14 @@ class NewCourseReviewForm extends React.Component {
     this.profOptions = [{label: 'Not Good', value: 1}, {label: 'Below Average', value: 2}, {label: 'Average', value: 3}, {label: 'Above Average', value: 4}, {label: 'Excellent!', value: 5}];
     this.state = {
       modalVisible: false,
-      start_year: '',
-      start_month: '',
-      workload_rating: '',
-      fairness_rating: '',
-      prof_rating: '',
-      overall_rating: '',
-      review_desc: '',
-      prof_name: '',
+      startMonth: '',
+      startYear: '',
+      workLoadRating: '',
+      fairnessRating: '',
+      profRating: '',
+      overallRating: '',
+      reviewDesc: '',
+      profName: '',
       descBoxHeight: 150
     };
     this.setModalVisible = this.setModalVisible.bind(this);
@@ -50,8 +50,8 @@ class NewCourseReviewForm extends React.Component {
     this.setState(obj)
   }
 
-  handleProfAutoSuggest(prof_name) {
-    this.setState({ prof_name });
+  handleProfAutoSuggest(profName) {
+    this.setState({ profName });
   }
 
   showPicker = async (stateKey, options) => {
@@ -60,18 +60,25 @@ class NewCourseReviewForm extends React.Component {
       var newState = {};
       const {action, year, month, day} = await DatePickerAndroid.open(options);
       if (action !== DatePickerAndroid.dismissedAction) {
-        this.setState({ start_year: year, start_month: months[month] });
+        this.setState({ startYear: year, startMonth: months[month] });
       }
     } catch ({code, message}) {
-      console.warn(`Error in example '${stateKey}': `, message);
+      console.log(`Error in example '${stateKey}': `, message);
     }
   };
 
   handleNewReview() {
-    let data = { ...this.state };
-    delete data.modalVisible;
-    delete data.descBoxHeight;
-
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let data = {
+      start_year: this.state.startYear,
+      start_month: this.state.startMonth,
+      workload_rating: this.state.workLoadRating,
+      fairness_rating: this.state.fairnessRating,
+      prof_rating: this.state.profRating,
+      overall_rating: this.state.overallRating,
+      review_desc: this.state.reviewDesc,
+      prof_name: this.state.profName,
+    };
     fetch(`http://127.0.0.1:19001/api/courses/${this.props.courseId}/reviews`, {
       method: 'POST',
       headers: {
@@ -99,49 +106,50 @@ class NewCourseReviewForm extends React.Component {
 
             <Text style={styles.modalHeader}>New Course Review:</Text>
 
-            <View style={styles.inputCotainer}>
+            <View style={styles.inputContainer}>
               <TouchableHighlight
                 hitSlop={{top: 50, bottom: 50, left: 50, right: 50}}
-                style={{position: 'absolute', right: 10, top: 10}}
+                style={{position: 'absolute', right: 10, top: 10, zIndex: 1}}
                 onPress={this.showPicker.bind(this, 'spinner', {date: new Date(), minDate: new Date(2007, 1, 1),
                 maxDate: new Date(), mode: 'spinner'})}>
-                <Text><FontAwesome name="calendar" size={30} color="black" /></Text>
+                <FontAwesome name="calendar" size={30} color="black" />
               </TouchableHighlight>
               <Text style={styles.inputLabel}>When did you start?</Text>
-              <Text style={styles.textInput}>{this.state.start_month} {this.state.start_year}</Text>
+              <Text style={styles.textInput}>{this.state.startMonth} {this.state.startYear}</Text>
             </View>
 
-            <View style={styles.inputCotainer}>
-              <Text style={styles.inputLabel}>How was the workload?</Text>
-              <RadioInput handleRadioChange={this.handleRadioChange} type="workload_rating" options={this.workLoadOptions} horizontal />
+            <View style={styles.inputContainer}>
+              <Text style={[styles.inputLabel, {paddingBottom: 5}]}>How was the workload?</Text>
+              <RadioInput handleRadioChange={this.handleRadioChange} type="workLoadRating" options={this.workLoadOptions} horizontal />
             </View>
 
-            <View style={styles.inputCotainer}>
-              <Text style={styles.inputLabel}>How was the evaluation?</Text>
-              <RadioInput handleRadioChange={this.handleRadioChange} type="fairness_rating" options={this.evalOptions} horizontal />
+            <View style={styles.inputContainer}>
+              <Text style={[styles.inputLabel, {paddingBottom: 5}]}>How was the evaluation?</Text>
+              <RadioInput handleRadioChange={this.handleRadioChange} type="fairnessRating" options={this.evalOptions} horizontal />
             </View>
 
-            <View style={styles.inputCotainer}>
-              <Text style={styles.inputLabel}>How was the instructor?</Text>
-              <RadioInput handleRadioChange={this.handleRadioChange} type="prof_rating" options={this.profOptions} />
+            <View style={styles.inputContainer}>
+              <Text style={[styles.inputLabel, {paddingBottom: 5}]}>How was the instructor?</Text>
+              <RadioInput handleRadioChange={this.handleRadioChange} type="profRating" options={this.profOptions} />
             </View>
 
-            <View style={styles.inputCotainer}>
+            <View style={styles.inputContainer}>
               <Text style={styles.inputLabel}>Overall satisfaction with the course?</Text>
-              <View style={styles.dividedRow}>
+              <View style={[styles.dividedRow, {paddingTop: 5}]}>
                 { [1, 2, 3, 4, 5].map(num =>
                   <View key={num} style={{flex: 1}}>
-                    <Text style={{textAlign: 'center'}} onPress={() => this.setState({ overall_rating: num })}>
-                      <FontAwesome name={this.state.overall_rating >= num ? "star" : "star-o"} size={25} />
-                    </Text>
+                    <FontAwesome
+                      name={this.state.overallRating >= num ? "star" : "star-o"}
+                      style={{textAlign: 'center', fontSize: 25}}
+                      onPress={() => this.setState({ overallRating: num })} />
                     <Text style={{textAlign: 'center'}}>{num}</Text>
                   </View>
                 )}
               </View>
             </View>
 
-            <View style={[styles.inputCotainer, {minHeight: 150}]}>
-              <Text style={styles.inputLabel}>Instructor's name (optional):</Text>
+            <View style={[styles.inputContainer, {minHeight: 135}]}>
+              <Text style={[styles.inputLabel, {paddingBottom: 5}]}>Instructor's name (optional):</Text>
               <AutoCompleteTextInput
                 placeholder="We'll auto-suggest some results :)"
                 data={this.props.profs}
@@ -149,13 +157,13 @@ class NewCourseReviewForm extends React.Component {
               />
             </View>
 
-            <View style={[styles.inputCotainer, {minHeight: 150}]}>
+            <View style={[styles.inputContainer, {minHeight: 150}]}>
               <Text style={styles.inputLabel}>Feel free to ellaborate (optional):</Text>
               <TextInput
                 style={[styles.textInput, {height: this.state.descBoxHeight}]}
                 multiline
-                onChangeText={review_desc => this.setState({review_desc})}
-                value={this.state.review_desc}
+                onChangeText={reviewDesc => this.setState({reviewDesc})}
+                value={this.state.reviewDesc}
                 placeholder="Provide context for your review (optional)..."
                 underlineColorAndroid="rgba(0,0,0,0)"
                 onContentSizeChange={event => {
@@ -164,19 +172,18 @@ class NewCourseReviewForm extends React.Component {
               />
             </View>
 
-            <View style={[styles.dividedRow, {marginTop: 10, marginBottom: 10}]}>
-              <View style={{flex: 1}}>
-                <Text onPress={this.handleNewReview} style={[styles.primaryBtn, {marginRight: 5}]}>
+             <View style={[styles.dividedRow, {marginBottom: 20}]}>
+              <View style={[styles.primaryBtnContainer, {marginRight: 5}]}>
+                <Text style={styles.primaryBtn} onPress={this.handleNewReview}>
                   Submit
                 </Text>
               </View>
-              <View style={{flex: 1}}>
-                <Text onPress={() => this.setModalVisible(false)} style={[styles.primaryBtn, {marginLeft: 5}]}>
-                  Go Back
+              <View style={[styles.primaryBtnContainer, {marginLeft: 5}]}>
+                <Text style={styles.primaryBtn} onPress={() => this.setModalVisible(false)}>
+                  Cancel
                 </Text>
               </View>
             </View>
-
 
           </ScrollView>
         </Modal>
@@ -184,7 +191,6 @@ class NewCourseReviewForm extends React.Component {
         <FontAwesome name="plus" style={this.props.style} onPress={() => this.setModalVisible(true)} />
 
       </View>
-
     );
   }
 
@@ -200,45 +206,50 @@ const styles = StyleSheet.create({
     color: '#004E89',
     fontWeight: 'bold',
     paddingBottom: 5,
+    marginBottom: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#004E89'
   },
-  inputCotainer: {
-    marginTop: 10,
+  modalHeader: {
+    color: '#004E89',
+    fontWeight: 'bold',
+    paddingBottom: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#004E89'
+  },
+  inputContainer: {
+    marginBottom: 10,
     padding: 5,
     borderWidth: .5,
     borderRadius: 5,
     borderColor: '#aaa'
   },
-  selectContainer: {
-    borderWidth: .5,
-    borderRadius: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    marginTop: 10,
-    borderColor: '#aaa'
-  },
   inputLabel: {
     color: '#004E89',
     fontWeight: 'bold',
-    marginBottom: 10
+    paddingTop: 2.5
+  },
+  textInput: {
+    minHeight: 30,
+    paddingTop: 2.5,
+    fontSize: 16
   },
   dividedRow: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  primaryBtnContainer: {
+    backgroundColor: '#004E89',
+    flex: 1,
+    borderRadius: 5,
+    borderColor: '#004E89',
+    borderWidth: .5,
+    padding: 5
+  },
   primaryBtn: {
     color: 'white',
-    backgroundColor: '#004E89',
-    padding: 5,
-    borderRadius: 5,
-    textAlign: 'center',
-    marginBottom: 10
+    textAlign: 'center'
   },
-  textInput: {
-    paddingRight: 5,
-    paddingLeft: 5
-  }
 });
-
